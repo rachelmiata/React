854d25aab70a1ba0d39fe5464449534e987ba5fe
Rachel Kirkpatrick <rachelmiata@gmail.com>
2021-10-15 18:19:13 -0500

    React Router Parameters

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
       return (
+        <BrowserRouter>
           <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
-              <Directory />
+              <Main />
           </div>
+        </BrowserRouter>
       );
   }
 }
